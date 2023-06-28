f9972f42d9b7282521194d618372df296dbcbe9f
jack <1908724366@qq.com>
2023-06-28 19:35:48 +0800

    初始化程序配置，引入ui库

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import Vue from 'vue'
-import App from './App'
-import './uni.promisify.adaptor'
+import Vue from 'vue';
+import App from './App';
+import './uni.promisify.adaptor';
 
-Vue.config.productionTip = false
+//引入并使用uView的JS库
+import uView from 'uview-ui';
+Vue.use(uView);
+Vue.config.productionTip = false;
 
-App.mpType = 'app'
+App.mpType = 'app';
 
 const app = new Vue({
   ...App
-})
-app.$mount()
+});
+app.$mount();
